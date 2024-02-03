1741021471c3924aaacf3860888f477d1aff78b6
85798xc <romanov454545@gmail.com>
2024-02-03 23:06:00 +0200

    start

---
+++ src/main/java/com/site/ChessLT/ChessLtApplication.java
@@ -0,0 +1,13 @@
+package com.site.ChessLT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessLtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessLtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/site/ChessLT/ChessLtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.site.ChessLT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessLtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
